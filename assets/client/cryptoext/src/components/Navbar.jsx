5802cbae02ee6c78b4b2f393328c8325f87206a8
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2023-01-29 20:10:14 +0100

    add logo && todo

--- assets/client/cryptoext/src/components/Navbar.jsx
+++ assets/client/cryptoext/src/components/Navbar.jsx
@@ -5,7 +5,10 @@ import '../App.css'
 const Navbar = () => {
   return (
     <div>
-      <ul>
+      <div className="logo">
+        <h1 className='logo'>CryptoExtension-Api</h1>
+      </div>
+      <ul className='navbar'>
         <li className='nav'>Home</li>
         <li className='nav'>About</li>
       </ul>
