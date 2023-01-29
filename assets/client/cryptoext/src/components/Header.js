b2e476d39fac03403e9f9204aeaac066ee5b4910
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2023-01-29 16:18:37 +0100

    test dans le css

--- assets/client/cryptoext/src/components/Header.js
+++ assets/client/cryptoext/src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../App.css'
 
 const Header = () => {
   return (
     <header>
-      <h1>Header Component</h1>
+      <h1 className='Test'>Header Component</h1>
     </header>
   );
 };
