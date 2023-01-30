5c79e48f7191a5d9d622e25bf70588cebd71780f
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2023-01-30 17:17:28 +0100

    link

--- assets/client/cryptoext/src/components/Navbar.jsx
+++ assets/client/cryptoext/src/components/Navbar.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import '../App.css'
 
 
+
 const Navbar = () => {
   return (
     <div className='tqt'>
-        <h1 className='logo'>CryptoExtension-Api</h1>
-      <ul className='navbar'>
-        <li>CryptoExtension</li>
+      <h1 className='logo'>CryptoExtension-Api</h1>
+       <ul className='navbar'>
+        <li><a href="https://cryptoextension.fr/" target="_blank" rel="noreferrer">CryptoExtension</a></li>
       </ul>
     </div>
   );
