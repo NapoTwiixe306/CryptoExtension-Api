711233e3e1bf1a5beb76fb2afc40729f5226343f
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2023-01-31 19:03:58 +0100

    raffale

--- assets/client/cryptoext/src/components/Header.jsx
+++ assets/client/cryptoext/src/components/Header.jsx
@@ -5,6 +5,7 @@ const Header = () => {
   return (
     <div className="header">
       <h1 className="header_title">Welcome To CryptoExtension-API !</h1>
+      <p className='paragraph'>CryptoExtension-Api, what’s it all about? let’s see</p>
     </div>
   );
 };
--- assets/client/cryptoext/src/components/Home.jsx
+++ assets/client/cryptoext/src/components/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import crypto from '../img/crypto.png'
 
 const Home = () => {
   return (
-    <header>
-      <h1 className='Test'>Home Component</h1>
-    </header>
+    <div className="home">
+      <div className="img_content">
+        <img src={crypto} alt='CryptoImg' className='crypto' />
+      </div>
+      <div className="text">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos autem ullam qui explicabo totam ut deserunt praesentium quisquam dolorum ipsa adipisci, et in, placeat sunt voluptatibus delectus. Iure, sequi laboriosam!</p>
+      </div>
+    </div>
+    
   );
 };
 
