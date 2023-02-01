35b8c0830bf258a6e9839728a0085588bf1ce0a4
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2023-02-01 22:53:24 +0100

    hh

--- assets/client/cryptoext/src/components/Home.jsx
+++ assets/client/cryptoext/src/components/Home.jsx
@@ -10,6 +10,11 @@ const Home = () => {
       <div className="text">
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos autem ullam qui explicabo totam ut deserunt praesentium quisquam dolorum ipsa adipisci, et in, placeat sunt voluptatibus delectus. Iure, sequi laboriosam!</p>
       </div>
+      <br></br>
+      <div className="text">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos autem ullam qui explicabo totam ut deserunt praesentium quisquam dolorum ipsa adipisci, et in, placeat sunt voluptatibus delectus. Iure, sequi laboriosam!</p>
+      </div>
+      
     </div>
     
   );
