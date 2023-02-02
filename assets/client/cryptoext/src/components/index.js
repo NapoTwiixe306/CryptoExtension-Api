350de20f0e6cbcc511085222a687762196e53d16
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2023-02-02 19:24:32 +0100

    delete react

--- assets/client/cryptoext/src/components/index.js
+++
@@ -1,6 +0,0 @@
-export {default as Header} from './Header';
-export {default as NavBar} from './Navbar';
-export {default as Home} from './Home';
-export {default as About} from './About';
-export {default as Footer} from './Footer';
-export {default as NotFound} from './NotFound';
